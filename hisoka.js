62793abe735d87d8a9ead4bdd8702965b2538f08
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-06-24 08:58:30 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -546,7 +546,7 @@ Silahkan @${m.mentionedJid[0].split`@`[0]} untuk ketik terima/tolak`
             }
             break
             case 'sc': {
-                m.reply('Script : https://github.com/DikaArdnt/Hisoka-Morou\n\n Dont Forget Give Star\n\nDonate : 6281615075793 (Link Aja)\nSaweria : https://saweria.co/DikaArdnt\nPaypal : https://www.paypal.me/Cakhaho\n\n Dont Forget Donate')
+                m.reply('Script : https://github.com/Rudal-XD/DarkBot\n\n Dont Forget Give Star\n\nDonate : 62895386194665 (Link Aja)\n\n Dont Forget Donate')
             }
             break
             case 'chat': {
