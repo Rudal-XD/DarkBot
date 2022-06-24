c607ae2214bea9ce8bf1fd3945126310b3e354c5
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-06-24 08:05:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Contact Me on wa.me/62895386194665
+   * Follow https://github.com/Rudal-XD
 */
 
 require('./config')
